08c8edb6a50a38ebcd1dcc8803bfe94683140c45
Rudrani26 <66739928+Rudrani26@users.noreply.github.com>
2023-04-24 23:10:51 +0530

    Delete ReplacewithHash.py

--- ReplacewithHash.py
+++
@@ -1,9 +0,0 @@
-def replaceWithHash(text):
-
-    for i in text.split():
-        if len(i) >= 5:
-            text = text.replace(i, "#" * len(i))
-    return text
-
-
-print(replaceWithHash("My name is Rudrani Yojak Chavarkar"))
