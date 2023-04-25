35d2cd0c7f70d5a661dff4e7b91d8ac223de1a7e
Rudrani26 <66739928+Rudrani26@users.noreply.github.com>
2023-04-25 11:23:16 +0530

    Star topology TCP Connection

---
+++ Networks Lab/TCPconnection.tcl
@@ -0,0 +1,46 @@
+set ns [new Simulator]
+
+set nf [open startopology.nam w]
+$ns namtrace-all $nf
+
+proc finish {} {
+global ns nf
+$ns flush-trace
+close $nf
+exec nam startopology.nam &
+exit 0
+}
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+
+$ns duplex-link $n0 $n2 10Mb 10ms DropTail
+$ns duplex-link $n1 $n2 10Mb 10ms DropTail
+$ns duplex-link $n2 $n3 10Mb 10ms DropTail
+
+$ns duplex-link-op $n0 $n2 orient right-down
+$ns duplex-link-op $n1 $n2 orient right-up
+$ns duplex-link-op $n2 $n3 orient right
+
+set tcp [new Agent/TCP]
+$tcp set class_ 2
+$ns attach-agent $n0 $tcp
+
+set sink [new Agent/TCPSink]
+$ns attach-agent $n3 $sink
+
+$ns connect $tcp $sink
+
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ftp set type_ FTP
+$ftp set packet_size_ 1000
+$ftp set rate_ 1mb
+
+$ns at 0.5 "$ftp start"
+$ns at 1.0 "$ftp stop"
+$ns at 1.5 "finish"
+
+$ns run
