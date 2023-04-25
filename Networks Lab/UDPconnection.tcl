0b53804173c5a21f2ed0d5b63ec8ad3319368305
Rudrani26 <66739928+Rudrani26@users.noreply.github.com>
2023-04-25 12:25:43 +0530

    UDP connection

---
+++ Networks Lab/UDPconnection.tcl
@@ -0,0 +1,45 @@
+set ns [new Simulator]
+
+set nf [open startopology.nam w]
+$ns namtrace-all $nf
+
+proc finish {} {
+global ns nf
+$ns flush-trace
+close $nf
+exec nam startopology.nam &
+exit 0
+}
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+
+$ns duplex-link $n0 $n2 10Mb 10ms DropTail
+$ns duplex-link $n1 $n2 10Mb 10ms DropTail
+$ns duplex-link $n2 $n3 10Mb 10ms DropTail
+
+$ns duplex-link-op $n0 $n2 orient right-down
+$ns duplex-link-op $n1 $n2 orient right-up
+$ns duplex-link-op $n2 $n3 orient right
+
+set udp [new Agent/UDP]
+$ns attach-agent $n1 $udp
+
+set null [new Agent/Null]
+$ns attach-agent $n3 $null
+
+$ns connect $udp $null
+
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp
+$cbr set type_ CBR
+$cbr set packet_size_ 1000
+$cbr set rate_ 1mb
+
+$ns at 0.5 "$udp start"
+$ns at 1.0 "$udp stop"
+$ns at 1.5 "finish"
+
+$ns run
