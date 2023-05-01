e8841835c552fd228aa5ddd04992fb504e7e0e7a
Rudrani26 <66739928+Rudrani26@users.noreply.github.com>
2023-05-01 13:39:14 +0530

    Factorial

---
+++ MP Lab/fact.asm
@@ -0,0 +1,38 @@
+.model small
+
+.data
+num dw 05h
+
+.code
+MOV ax, @data 				; initialize the data segment
+MOV ds, ax
+
+MOV ax, 01 					; initialize ax = 1
+MOV bx, num 				; load the number in bx
+
+CALL fact 					; call procedure
+MOV di, ax 					; store lsb of result in di
+MOV bp, 2 					; initialize count for no of times display is called
+MOV bx, dx 					; store msb of result in reg bx
+MOV bx, di 					; store lsb of result in bx
+DEC bp 					; decrement bp
+MOV ah,4ch
+Int 21h
+
+Fact proc near 				; function for finding the factorial
+CMP bx,01 					; if bx=1
+JZ loop1 					; if yes ax=1
+
+loop2: 
+MUL bx 					; find factorial
+DEC bx 					; decrement bx
+CMP bx,01 					; multiply bx=1
+JNE loop2
+RET
+
+loop1:
+MOV ax,01 					; initialize ax=1
+RET 						; return to called program
+
+fact ENDP 					; end procedure
+END 						; end program
